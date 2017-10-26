9a24423566a87f7b480f8c06340810b074be9f31
Aditya Neralkar <neralkarad15.it@coep.ac.in>
2017-10-27 03:16:53 +0530

    registration

--- fl.py
+++ fl.py
@@ -17,19 +17,48 @@ def login_page(name=None):
 def login_page_post(name=None):
     username, password = request.form['username'],request.form['password']
     try:
-        cursor.execute(("select password from users where name ='{}'".format(username)))
+        cursor.execute(("select password from users where name ='{}';".format(username)))
     except:
         cursor.execute(("rollback;"))
-    cursor.execute(("select password from users where name ='{}'".format(username)))
+    cursor.execute(("select password from users where name ='{}';".format(username)))
     a = cursor.fetchone()
     if not a:
         return ("Incorrect username")
     if a[0] == password:
         return ("Hello {}, you've successfully logged in".format(username))
-    else
+    else:
         return ("Incorrect Password")
 
+@app.route('/registration.html')
+def registration_page(name=None):
+    return render_template('registration.html', name=name)
 
+@app.route('/registration.html', methods=['POST','GET'])
+def registration_page_post(name=None):
+    firstname = request.form['firstname']
+    lastname = request.form['lastname']
+    username = request.form['username']
+    email = request.form['emailid']
+    fav = request.form['favouriteteam']
+    password = request.form['password']
+    cpassword = request.form['cpassword']
+    if password != cpassword:
+        return ("Password does not match")
+    try:
+        cursor.execute(("select password from users where name ='{}';".format(username)))
+    except:
+        cursor.execute(("rollback;"))
+    cursor.execute(("select password from users where name ='{}';".format(username)))
+    a = cursor.fetchone()
+    if not a:
+        try:
+            cursor.execute(("insert into users(name, password) values('{}', '{}');".format(username, password)))
+        except:
+            cursor.execute(("rollback;"))
+        cursor.execute(("insert into users(name,password) values('{}', '{}');".format(username, password)))
+        return ("Hello {}, you've successfully registered".format(username))
+    else:
+        return ("Username is already taken!")
 
 
 
